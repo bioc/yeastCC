cd7e6e7ce747b2dc8a0ba1a1e3c97e4f65d32146
Nianhua Li <nli@fhcrc.org>
2006-09-27 20:10:15 +0000

    problem: can not load package on windows solution: use lazy data loading
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/yeastCC@140 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,4 @@
  require(Biobase)
  path = .path.package(pkgname)
  where <- as.environment(match(paste("package:", pkgname, sep=""), search()))
- for(i in c("yeastCC"))
-   load(file.path(path, "data", paste(i, ".rda", sep="")), envir=where)
 }
